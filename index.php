f65eb80b489eeee23ef2e6f8fd3a371b4562890e
Taylor Meyer <tam008@g.harvard.edu>
2018-02-18 22:49:14 -0500

    adding basic HTML

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang='en'>
+<head>
+    <meta charset='utf-8'>
+
+    <title>The Magic Bill Splitter</title>
+    <meta name='description' content='The Magic Bill Splitter'>
+    <meta name='author' content='Taylor Meyer Site'
+
+    <link rel='stylesheet' href='styles.css'>
+
+</head>
+
+<body>
+
+</body>
+</html>
